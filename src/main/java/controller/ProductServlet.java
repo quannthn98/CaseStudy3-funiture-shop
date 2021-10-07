8cabaaf8ba2902b663f24147dad57e5544338df3
vanhopld98 <vanhopld98@gmail.com>
2021-10-07 13:50:25 +0700

    Xy Ly O Select

--- src/main/java/controller/ProductServlet.java
+++ src/main/java/controller/ProductServlet.java
@@ -1,6 +1,10 @@
 package controller;
 
+import dao.BrandDao;
+import dao.CategoryDao;
 import dao.ProductDao;
+import model.Brand;
+import model.Category;
 import model.Product;
 
 import javax.servlet.*;
@@ -12,6 +16,8 @@ import java.util.List;
 @WebServlet(name = "ProductServlet", value = "/product")
 public class ProductServlet extends HttpServlet {
     ProductDao productDAO = new ProductDao();
+    BrandDao brandDao = new BrandDao();
+    CategoryDao categoryDao = new CategoryDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -88,6 +94,10 @@ public class ProductServlet extends HttpServlet {
     }
 
     private void showFormCrateProduct(HttpServletRequest request, HttpServletResponse response) {
+        List<Brand> brandList = brandDao.getAll();
+        List<Category> categoryList = categoryDao.getAll();
+        request.setAttribute("brandList", brandList);
+        request.setAttribute("categoryList", categoryList);
         RequestDispatcher dispatcher = request.getRequestDispatcher("product/create.jsp");
         try {
             dispatcher.forward(request, response);
